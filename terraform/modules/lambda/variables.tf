b2793e3fc0e1fbf071f169367b033caed81bfb71
maishio <masanori.ishio119@gmail.com>
2023-05-02 17:53:41 +0900

    refactor: remove iam_role_arn, teams_webhook_url variables

--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -3,17 +3,12 @@ variable "account" {
   type        = map(string)
 }
 
-variable "iam_role_arn" {
-  description = "ARN of the IAM role to assume."
+variable "slack_webhook_url" {
+  description = "Slack webhook URL."
   type        = string
 }
 
 variable "tags" {
   description = "A map of tags to add to all resources."
   type        = map(string)
-}
-
-variable "teams_webhook_url" {
-  description = "URL of the Teams webhook."
-  type        = string
 }
