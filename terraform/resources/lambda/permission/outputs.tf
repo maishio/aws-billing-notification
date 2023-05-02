f70cc08311cffbc846418b0355d226be5d6637a8
maishio <masanori.ishio119@gmail.com>
2023-05-03 07:03:53 +0900

    refactor: remove lambda permission resource

--- terraform/resources/lambda/permission/outputs.tf
+++
@@ -1,6 +0,0 @@
-# AWS Lambda Permission
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_permission
-
-output "lambda_permission" {
-  value = aws_lambda_permission.this
-}
