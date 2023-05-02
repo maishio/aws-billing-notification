78437d0ab6ada9404534d218097fc7937577bde3
maishio <masanori.ishio119@gmail.com>
2023-05-03 07:41:53 +0900

    refactor: remove ecs cluster resource

--- terraform/resources/ecs/cluster/outputs.tf
+++
@@ -1,7 +0,0 @@
-# Amazon ECS Cluster
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_cluster
-
-output "ecs_cluster" {
-  description = "Amazon ECS Cluster resource."
-  value       = aws_ecs_cluster.this
-}
