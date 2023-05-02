a3318e636c00c5aafad0158e9ed6712efb90501a
maishio <masanori.ishio119@gmail.com>
2023-05-02 17:19:30 +0900

    refactor: remove lambda_layer module

--- terraform/modules/lambda/lambda_layer.tf
+++
@@ -1,7 +0,0 @@
-module "lambda_layer" {
-  source              = "../../resources/lambda_layer"
-  compatible_runtimes = ["python3.9"]
-  filename            = data.archive_file.library.output_path
-  layer_name          = "${var.tags.service}-${var.tags.env}-billing-notification-function"
-  source_code_hash    = data.archive_file.library.output_base64sha256
-}
