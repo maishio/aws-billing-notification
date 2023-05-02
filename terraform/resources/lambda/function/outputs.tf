3ee5fbda21ae36ca09036d20f84dec7f14003a5a
maishio <masanori.ishio119@gmail.com>
2023-05-03 07:03:23 +0900

    refactor: remove lambda function resource

--- terraform/resources/lambda/function/outputs.tf
+++
@@ -1,6 +0,0 @@
-# AWS Lambda Function
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function
-
-output "lambda_function" {
-  value = aws_lambda_function.this
-}
