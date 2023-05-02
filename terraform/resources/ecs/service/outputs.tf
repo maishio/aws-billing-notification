2e218967444a659e54ccd8e366e57f9ab9e6058b
maishio <masanori.ishio119@gmail.com>
2023-05-03 07:42:31 +0900

    refactor: remove ecs service resource

--- terraform/resources/ecs/service/outputs.tf
+++
@@ -1,7 +0,0 @@
-# Amazon ECS Service
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_service
-
-output "ecs_service" {
-  description = "Amazon ECS Service resource."
-  value       = aws_ecs_service.this
-}
