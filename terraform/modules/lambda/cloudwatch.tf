31364375685fe4a396f6c719ae5e634eb8382be3
maishio <masanori.ishio119@gmail.com>
2023-05-04 20:36:59 +0900

    refactor: rename cloudwatch log group name

--- terraform/modules/lambda/cloudwatch.tf
+++ terraform/modules/lambda/cloudwatch.tf
@@ -1,6 +1,6 @@
 module "cloudwatch" {
   source            = "../../resources/cloudwatch/log_group"
-  name              = "/aws/lambda/notify-aws-billing"
+  name              = "/aws/lambda/${var.tags.service}-${var.tags.env}-notify-aws-billing"
   retention_in_days = 7
   tags              = var.tags
 }
