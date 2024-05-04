29fe89f03f6d7c3f28fe02dbd37ea34a506cbc83
maishio <masanori.ishio119@gmail.com>
2024-05-04 22:17:55 +0900

    refactor: remove

--- config.ts
+++
@@ -1,7 +0,0 @@
-export const AWS_REGION = process.env.AWS_REGION || "us-east-1"
-export const CDKTF_BACKEND_BUCKET = process.env.CDKTF_BACKEND_BUCKET || "your-s3-bucket-name"
-export const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL || "your-slack-webhook-url"
-export const TAGS = {
-  service: "aws-billing",
-  env: "prod"
-}
